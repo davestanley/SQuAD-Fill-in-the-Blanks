9fde0eaa94a3bf2d78dea3b5d42e288cde9de2c5
David Stanley <davearthur.stanley@gmail.com>
2019-02-20 19:20:16 -0500

    Finished 1st pass through writing run_merge ,for merging classified blanks with original SQuAD dataset

--- run_blanks_from_training.py
+++ run_blanks_from_training.py
@@ -11,6 +11,7 @@ sys.path.insert(0, os.path.join(curr_folder,'modules'))
 from utils_EDA import p_list_qas
 from utils import load_SQuAD_train
 from utils import load_SQuAD_dev
+from utils_SQuAD import classify_blanks_from_answers
 
 # Load the training data
 arts_train = load_SQuAD_train()
@@ -24,40 +25,30 @@ Ndev = len(arts_dev)
 print ("Narticles in train = " +  str(len(arts_train)))
 print ("Narticles in dev = " +  str(len(arts_dev)))
 
+# Settings
+maxWords_per_FITB = 2            # We conly consider answers with at most this many words for blanking out
+
 # # TRAINING DATASET # #
-# # Pick out a subset of articles
 art = arts_train[:]
-# art = arts_train[14:15]
 
-from utils_SQuAD import classify_blanks_from_answers
-
-maxWords_per_FITB = 2
+# Run the classifier
 art3 = classify_blanks_from_answers(art,maxWords_per_FITB=2,return_full=False)
 
 # Do a test print
 print(art3[0]['title'])
 print(art3[0]['paragraphs'][0]['context_blanked'])
 
-
-
-
-
 # # Save the file
-
 from utils import get_foldername, save_data
 foldername = get_foldername('sq_pp_training')
 save_data(art3,'train.json',foldername);
 
 
 
-
 # # DEV DATASET # #
-# # Pick out a subset of articles
 art = arts_dev[:]
 
-from utils_SQuAD import classify_blanks_from_answers
-
-maxWords_per_FITB = 2
+# Run the classifier
 arts3dev = classify_blanks_from_answers(art,maxWords_per_FITB=2,return_full=False)
 
 # Do a test print
--- run_merge.py
+++ run_merge.py
@@ -18,10 +18,23 @@ from utils import get_foldername
 from utils import merge_artfiles
 from utils_EDAplots import plotbar_train_dev,plothist_train_dev
 
-# Load blanks data (ground truth)
+# Path for saving data
+foldername_save = '.'            # Save in current folder
+
+# Load the original data
+arts_train = load_SQuAD_train()
+arts_dev = load_SQuAD_dev()
+
+
+# Load blanks data (ground truth) [call this arts3]
 foldername = get_foldername('sq_pp_training')
 arts3train = load_data('train.json',foldername)
 arts3dev = load_data('dev.json',foldername)
+
+
+# # # # # # Training data # # # # # #
+arts = arts_train
+arts3 = arts3train
 
 # Make sure all titles match
 all_title_pairs = [(a1['title'],a3['title']) for a1,a3 in zip(arts,arts3)]
@@ -31,4 +44,25 @@ print("Matching titles: {} \nTotal articles {}".format(sum(titles_match_bool),le
 # Merge ground truth blanks with original data to get full dataset
 from utils_SQuAD import merge_arts_paragraph_fields
 list_of_fields = ['context_blanked','blank_classification']
-arts = merge_arts_paragraph_fields(arts,arts3,list_of_fields)
+arts_merged = merge_arts_paragraph_fields(arts,arts3,list_of_fields)
+
+# Finally, save the data
+save_data(arts_merged,'train.json',foldername_save);
+
+
+# # # # # # Dev data # # # # # #
+arts = arts_dev
+arts3 = arts3dev
+
+# Make sure all titles match
+all_title_pairs = [(a1['title'],a3['title']) for a1,a3 in zip(arts,arts3)]
+titles_match_bool = [a1['title'] == a3['title'] for a1,a3 in zip(arts,arts3)]
+print("Matching titles: {} \nTotal articles {}".format(sum(titles_match_bool),len(titles_match_bool)))
+
+# Merge ground truth blanks with original data to get full dataset
+from utils_SQuAD import merge_arts_paragraph_fields
+list_of_fields = ['context_blanked','blank_classification']
+arts_merged = merge_arts_paragraph_fields(arts,arts3,list_of_fields)
+
+# Finally, save the data
+save_data(arts_merged,'dev.json',foldername_save);
